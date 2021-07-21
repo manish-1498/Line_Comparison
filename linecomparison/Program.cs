1ac78891a232dc055e17f9364121248703bee867
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-21 18:02:42 +0530

    [manish]:added:comparison of files

--- linecomparison/Program.cs
+++ linecomparison/Program.cs
@@ -6,7 +6,7 @@ namespace linecomparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparison program");
+
             /*first parameter is x1 then x2 then y1 then y2 
              * then x3 then x4 then y3 then y4*/
             linecomparison line = new linecomparison(1,3,5,7,1,1,1,1);
--- linecomparison/linecomparison.cs
+++ linecomparison/linecomparison.cs
@@ -9,6 +9,7 @@ namespace linecomparison
         int x1, x2,x3,x4, y1, y2,y3,y4;
         public linecomparison(int x1, int x2, int y1, int y2, int x3, int x4, int y3, int y4)
         {
+            Console.WriteLine("Welcome to line comparison program");
             this.x1 = x1;
             this.x2 = x2;
             this.y1 = y1;
