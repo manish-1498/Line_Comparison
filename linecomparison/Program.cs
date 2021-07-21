48438f93343480bf6e32f46d3e616e8409dedb66
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-21 16:20:41 +0530

    [manish]:added:equality of lines

--- linecomparison/Program.cs
+++ linecomparison/Program.cs
@@ -6,7 +6,9 @@ namespace linecomparison
     {
         static void Main(string[] args)
         {
-            linecomparison line = new linecomparison(1,3,5,7);
+            /*first parameter is x1 then x2 then x3 then x4 
+             * then y1 then y2 then y3 then y4*/
+            linecomparison line = new linecomparison(1,3,5,7,1,3,5,7);
         }
     }
 }
--- linecomparison/linecomparison.cs
+++ linecomparison/linecomparison.cs
@@ -6,15 +6,33 @@ namespace linecomparison
 {
     class linecomparison
     {
-        int x1, x2, y1, y2;
-        public linecomparison(int x1,int x2,int y1,int y2)
+        int x1, x2,x3,x4, y1, y2,y3,y4;
+        public linecomparison(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4)
         {
             this.x1 = x1;
             this.x2 = x2;
             this.y1 = y1;
             this.y2 = y2;
-            double lengthofline = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2));
-            Console.WriteLine("length of line is:"+lengthofline);
+            this.x3 = x3;
+            this.x4 = x4;
+            this.y3 = y3;
+            this.y4 = y4;
+            Console.WriteLine("value of x1 =" + x1);
+            Console.WriteLine("value of x2 =" + x2);
+            Console.WriteLine("value of x3 =" + x3);
+            Console.WriteLine("value of x4 =" + x4);
+            Console.WriteLine("value of y1 =" + y1);
+            Console.WriteLine("value of y2 =" + y2);
+            Console.WriteLine("value of y3 =" + y3);
+            Console.WriteLine("value of y4 =" + y4);
+            double lengthofline1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length of line one is:" + lengthofline1);
+            double lengthofline2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("length of line two is :" + lengthofline2);
+            if(lengthofline1==lengthofline2)
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("lines are unequal");
         }
     }
 }
