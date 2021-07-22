8ff96942005022f3eebfcc0633c1368ac6dc621b
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-22 20:28:39 +0530

    [manish]:added:equality of two lines

--- linecomparison/Program.cs
+++ linecomparison/Program.cs
@@ -6,10 +6,18 @@ namespace linecomparison
     {
         static void Main(string[] args)
         {
-
-            /*first parameter is x1 then x2 then y1 then y2 
-             * then x3 then x4 then y3 then y4*/
-            linecomparison line = new linecomparison(1,3,5,7,1,1,1,1);
+            //first parameter is x1 then x2 then y1 then y2 
+            linecomparison Line1 = new linecomparison(1,3,5,7);
+            double Length1 = Line1.Linecomp();
+            Console.WriteLine("length of line 1 ="+Length1);
+            linecomparison Line2  = new linecomparison(2,3,4,5);
+            double Length2 = Line2.Linecomp();
+            Console.WriteLine("length of line 2 =" + Length2);
+            Console.WriteLine("----------------------------");
+            if (Length1 == Length2)
+                Console.WriteLine("Result--Both lines are equal");
+            else
+                Console.WriteLine("Result--lines are unequal");
         }
     }
 }
--- linecomparison/linecomparison.cs
+++ linecomparison/linecomparison.cs
@@ -6,42 +6,23 @@ namespace linecomparison
 {
     class linecomparison
     {
-        int x1, x2,x3,x4, y1, y2,y3,y4;
-        public linecomparison(int x1, int x2, int y1, int y2, int x3, int x4, int y3, int y4)
+        int x1, x2, y1, y2;
+        public linecomparison(int x1, int x2, int y1, int y2)
         {
-            Console.WriteLine("Welcome to line comparison program");
             this.x1 = x1;
             this.x2 = x2;
             this.y1 = y1;
             this.y2 = y2;
-            this.x3 = x3;
-            this.x4 = x4;
-            this.y3 = y3;
-            this.y4 = y4;
+        }
+        public double Linecomp()
+        {
+            Console.WriteLine("---------------------------------------");
             Console.WriteLine("value of x1 =" + x1);
             Console.WriteLine("value of x2 =" + x2);
-            Console.WriteLine("value of x3 =" + x3);
-            Console.WriteLine("value of x4 =" + x4);
             Console.WriteLine("value of y1 =" + y1);
             Console.WriteLine("value of y2 =" + y2);
-            Console.WriteLine("value of y3 =" + y3);
-            Console.WriteLine("value of y4 =" + y4);
-            double lengthofline1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("length of line one is:" + lengthofline1);
-            double lengthofline2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
-            Console.WriteLine("length of line two is :" + lengthofline2);
-            if (lengthofline1 == lengthofline2)
-            {
-                Console.WriteLine("Both lines are equal");
-            }
-            else if (lengthofline1 > lengthofline2)
-            {
-                Console.WriteLine("length of first line is greater than second line");
-            }
-            else
-            {
-                Console.WriteLine("lines are not equal and length of first line is less than second line");
-            }
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return length;
         }
     }
 }
