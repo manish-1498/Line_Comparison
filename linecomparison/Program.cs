4523b94b81ee270ebf9f76c859875ffbb95dfd85
manish-1498 <manishpradhan14121998@gmail.com>
2021-07-26 15:48:02 +0530

    [manish]:Added:compare to method

--- linecomparison/Program.cs
+++ linecomparison/Program.cs
@@ -7,16 +7,16 @@ namespace linecomparison
         static void Main(string[] args)
         {
             //first parameter is x1 then x2 then y1 then y2 
-            linecomparison Line1 = new linecomparison(10,20,30,40);
+            linecomparison Line1 = new linecomparison(10,23,30,40);
             double Length1 = Line1.Linecomp();
             Console.WriteLine("length of line 1 ="+Length1);
             linecomparison Line2  = new linecomparison(5,15,25,35);
             double Length2 = Line2.Linecomp();
             Console.WriteLine("length of line 2 =" + Length2);
             Console.WriteLine("----------------------------");
-            if (Length1 == Length2)
+            if (Length1.CompareTo(Length2)==0)
                 Console.WriteLine("Result--Both lines are equal");
-            else if (Length1 > Length2)
+            else if (Length1.CompareTo(Length2)>0)
             {
                 Console.WriteLine("Result--length of first line is greater than second line");
             }
